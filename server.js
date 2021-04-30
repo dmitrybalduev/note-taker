5384e0fd954bfd00670177d43f48dc8e29bef802
Dmitry <dmitry.balduev@gmail.com>
2021-04-30 18:06:31 -0400

    added api/notes post method, added logic to handle empty db when adding first note

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const app = express();
 const PORT = 3000;
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -13,14 +14,38 @@ app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/note
 
 app.get("/api/notes",  (req, res) => {
     fs.readFile("db/db.json", "utf8", function (err, notes) {
-        if (err) {
-            console.log(err);
-            return;
+        if (err) throw err;
+        if(notes){
+            res.json(JSON.parse(notes)); 
+        } else{
+            res.json("{}");
         }
-        res.json(JSON.parse(notes)); 
+        
     })
 });
 
+app.post("/api/notes",  (req, res) => {
+    const note = req.body;
+    let notesArray;
+    let id;
+    fs.readFile(path.join(__dirname + "/db/db.json"), "utf8", (err, data) => {
+        if (err) throw err;
+        notesArray = data.length === 0? []: JSON.parse(data);
+        // id = notesArray[notesArray.length - 1].id + 1;
+        id = notesArray.length === 0 ? 1: notesArray[notesArray.length - 1].id + 1;
+        let newObj = {
+            id: id,
+            title: note.title,
+            text: note.text,
+        }
+        notesArray.push(newObj);
+
+        fs.writeFile((path.join(__dirname + "/db/db.json")), JSON.stringify(notesArray),  (error) => {
+            if (error) throw error; 
+            res.json(newObj);
+        });
+    });
+});
 
 
 
